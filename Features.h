50597e3f62c68b06f9d8a24a72cabd472ccbc4b0
Vitaliy <46225587+Pacmanzbs@users.noreply.github.com>
2020-01-03 14:53:00 +0300

    Update Features.h

--- Features.h
+++ Features.h
@@ -2,10 +2,10 @@
 #include <vector>
 
 std::vector <int> FindWord(std::string text, std::string word);
-<<<<<<< HEAD
+
 
 int CheckBracket(std::string b);
-=======
+
 int CheckBracket(std::string text);
 std::string GetSpace(std::string& text);
->>>>>>> 2f88629cc99e057d7a5e7be2d1c89195ec788783
+
